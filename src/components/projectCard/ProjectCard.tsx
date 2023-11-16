ac11ed5dd10880281d6c88b0b8b3127acff50d30
Kritarth Sharma <kritarth2121@gmail.com>
2023-11-17 01:51:56 +0530

    added more projects

--- src/components/projectCard/ProjectCard.tsx
+++ src/components/projectCard/ProjectCard.tsx
@@ -26,31 +26,31 @@ export default function ProjectCard({ repo, theme }: any): React.JSX.Element {
             setVisible(true);
           }}
         >
-            <div className="relative  overflow-hidden">
-              <img
-                className="w-full  h-full hover:scale-110 transition-all duration-500 cursor-pointer"
-                src={require(`../../assests/images/work-images/${repo["homeImage"]}`)}
-                alt=""
-              />
-            </div>
-            <div className="flex flex-row space-x-4 items-center justify-center">
-              <svg
-                aria-hidden="true"
-                className="octicon repo-svg"
-                height="16"
-                role="img"
-                viewBox="0 0 12 16"
-                width="12"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z"
-                ></path>
-              </svg>
-              <p className="repo-name" style={{ color: theme.text }}>
-                {repo.name}
-              </p>
-            </div>
+          <div className="relative  overflow-hidden">
+            <img
+              className="w-full  h-full hover:scale-110 transition-all duration-500 cursor-pointer"
+              src={require(`../../assests/images/work-images/${repo["homeImage"]}`)}
+              alt=""
+            />
+          </div>
+          <div className="flex flex-row space-x-4 items-center justify-center">
+            <svg
+              aria-hidden="true"
+              className="octicon repo-svg"
+              height="16"
+              role="img"
+              viewBox="0 0 12 16"
+              width="12"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z"
+              ></path>
+            </svg>
+            <p className="repo-name" style={{ color: theme.text }}>
+              {repo.name}
+            </p>
+          </div>
         </div>
         <Modal
           visible={visible}
@@ -83,7 +83,14 @@ export default function ProjectCard({ repo, theme }: any): React.JSX.Element {
             </Carousel>
             <div className="mx-6">
               <p className="repo-description " style={{ color: theme.text }}>
-                {repo.description}
+                {repo.description.map((item, i) => {
+                  return (
+                    <div>
+                      {item} <br />
+                      <br />
+                    </div>
+                  );
+                })}
               </p>
               {repo?.liveUrl && (
                 <a href={repo?.liveUrl} target="_blank">
